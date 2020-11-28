90fea97c80c0708e3c2d73999e49e339275f0d1b
Steve <stephenvincentibanez@gmail.com>
2020-11-28 13:44:57 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// let totalBatteries = () => {
+// return batteryBatches.reduce((total, batteries) => batteries + total, 0)}
+
+let totalBatteries = batteryBatches.reduce( (total, batteries) => batteries + total, 0)
